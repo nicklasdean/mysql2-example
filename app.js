5ec2eeef1885e58ef677a9c47779e54f4c6cf0ef
dean <nicklasfrederiksen@gmail.com>
2023-11-24 09:22:37 +0100

    first

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
+//npm install mysql2 --save
 const mysql = require("mysql2");
+//npm install cors --save
 const cors = require("cors");
 
 const app = express();
@@ -7,13 +9,30 @@ const port = 3000;
 
 app.use(cors());
 
-app.get('/',(req, res)=>{
-    //Host, user, password database
-    const connection = mysql.createConnection({})
-    connection.query('SELECT `name` FROM pokemon',(error,results)=>{});
+//Host, user, password database
+const connection = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"hejsa",
+    database:"pokemon"
 });
 
-app.get('/pokemon/:name/', (req,res)=>{});
+app.get('/',(req, res)=>{
+    connection.query('SELECT `name` FROM pokemon',(error,results)=>{
+        res.send(results);
+    });
+});
+
+//Find single pokemon by name
+app.get('/pokemon/:name/', (req,res)=>{
+    const pokemonUserRequest = req.params.name;
+    connection.query('SELECT * FROM pokemon WHERE ´name´ = ?',
+    [pokemonUserRequest],
+    (error, results)=>{
+        res.send(results);
+    });
+
+});
 
 app.get('/pokedexnumber/:name/', (req,res)=>{});
 
